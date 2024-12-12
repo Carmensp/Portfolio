2e2ca8754abdb759eef51c7c6f74d0a693f3a95b
carmensp <carmen.8.2.0.3.sp@gmail.com>
2024-12-12 18:31:56 +0100

    tercer commit

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -1,12 +1,8 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import myImg from "../../Assets/avatar.svg";
+import myImg from "../../Assets/myAvatar.svg";
 import Tilt from "react-parallax-tilt";
-import {
-  AiFillGithub,
-  AiOutlineTwitter,
-  AiFillInstagram,
-} from "react-icons/ai";
+import { AiFillGithub } from "react-icons/ai";
 import { FaLinkedinIn } from "react-icons/fa";
 
 function Home2() {
